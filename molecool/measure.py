0160a1f325ebd8bbe3605b7a242c0570f946b138
pkiruba <kirubanpk@gmail.com>
2020-05-15 16:00:16 -0400

    docstring added to measure.py

--- molecool/measure.py
+++ molecool/measure.py
@@ -6,7 +6,27 @@ import numpy as np
 
 
 def calculate_distance(rA, rB):
-    # This function calculates the distance between two points given as numpy arrays.
+    """
+    Calculate the distance between two points.
+
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+        The coordinates of each point.
+
+    Returns
+    --------
+    distance : float
+        The distance between the two points.
+
+    Examples
+    --------
+    >>> r1 = np.array([0.0, 0.0, 0.0])
+    >>> r2 = np.array([0.0, 0.0, 0.0])
+    >>> calculate_distance(r1, r2)
+    1.0
+    """
+
     if isinstance(rA, np.ndarray) is False or isinstance(rB, np.ndarray) is False:
         raise TypeError("rA and rB must be numpy arrays")
     d = (rA-rB)
